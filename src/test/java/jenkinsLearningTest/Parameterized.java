07544d49aab03489f1e364d9894ddeb157038820
Rajeshluffy <rajeshveeramani12@gmail.com>
2026-08-16 22:52:39 +0530

    driver option

--- src/test/java/jenkinsLearningTest/Parameterized.java
+++ src/test/java/jenkinsLearningTest/Parameterized.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Parameterized {
 	@Test
@@ -13,7 +16,22 @@ public class Parameterized {
 		String browser = System.getProperty("env", "chrome");
 		switch (browser) {
 		case "chrome":
-			driver=new ChromeDriver();
+			ChromeOptions options = new ChromeOptions();
+
+// 1. Run without a GUI (Mandatory for Docker)
+options.addArguments("--headless=new"); 
+
+// 2. Disable sandbox security (Mandatory because Docker runs as root)
+options.addArguments("--no-sandbox"); 
+
+// 3. Prevent Chrome from crashing due to Docker's limited /dev/shm memory
+options.addArguments("--disable-dev-shm-usage"); 
+
+// 4. (Optional but recommended) Set a default window size for headless testing
+options.addArguments("--window-size=1920,1080"); 
+
+ 
+			driver=new ChromeDriver(options);
 			break;
 		case "edge":
 			driver = new EdgeDriver();
--- src/test/java/jenkinsLearningTest/SimpleTest.java
+++ src/test/java/jenkinsLearningTest/SimpleTest.java
@@ -1,6 +1,9 @@
 package jenkinsLearningTest;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,7 +13,22 @@ public class SimpleTest {
 	public void login() {
 		//Chrome Driver improvements
 		// Poll scm trigger
-		driver = new ChromeDriver();
+ChromeOptions options = new ChromeOptions();
+
+// 1. Run without a GUI (Mandatory for Docker)
+options.addArguments("--headless=new"); 
+
+// 2. Disable sandbox security (Mandatory because Docker runs as root)
+options.addArguments("--no-sandbox"); 
+
+// 3. Prevent Chrome from crashing due to Docker's limited /dev/shm memory
+options.addArguments("--disable-dev-shm-usage"); 
+
+// 4. (Optional but recommended) Set a default window size for headless testing
+options.addArguments("--window-size=1920,1080"); 
+
+
+		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.get("https://www.google.co.in/");
 		String pageTitle = driver.getTitle();
